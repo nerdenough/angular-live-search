5788d7f67419b28797a5c39f061098e0a1912ab9
Brendan Goodenough <brendan@goodenough.nz>
2016-03-16 20:23:55 +1300

    Tidy up code and comments :heart:

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var app = angular.module('searchModule', []);
-
-// Defines the search controller by bringing the data into the scope.
-//
 app.controller('searchController', function($scope) {
   $scope.data = data;
 
+  // Sets the text of the search box and hides the drop down.
   $scope.setQuery = function(query) {
     $scope.query = query;
     $scope.focus = false;
@@ -12,7 +10,6 @@ app.controller('searchController', function($scope) {
 });
 
 // Returns the search function that will perform the filter on the data.
-//
 app.filter('search', function() {
   return search;
 });
@@ -20,7 +17,6 @@ app.filter('search', function() {
 // Returns an array of items where the item text matches the search query. In
 // this example, both the query and item are converted to lower case for easier
 // matching.
-//
 function search(arr, query) {
   if (!query) {
     return arr;
