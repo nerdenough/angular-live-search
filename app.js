50f04a6b344f4d74ce6d0c34d746f5c0c1443c04
Brendan Goodenough <brendan@goodenough.nz>
2016-03-16 19:28:51 +1300

    Show search results with filtering

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 var app = angular.module('searchModule', []);
 
-app.controller('searchController', function ($scope) {
+// Defines the search controller by bringing the data into the scope.
+//
+app.controller('searchController', function($scope) {
   $scope.data = data;
 });
+
+// Returns the search function that will perform the filter on the data.
+//
+app.filter('search', function() {
+  return search;
+});
+
+// Returns an array of items where the item text matches the search query. In
+// this example, both the query and item are converted to lower case for easier
+// matching.
+//
+function search(arr, query) {
+  if (!query) {
+    return arr;
+  }
+
+  var results = [];
+  query = query.toLowerCase();
+
+  angular.forEach(arr, function(item) {
+    if (item.toLowerCase().indexOf(query) !== -1) {
+      results.push(item);
+    }
+  });
+
+  return results;
+};
